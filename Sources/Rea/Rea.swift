56ba44115f60a6b498b5f4c2dd7af41f998f2612
Juan David Hurtado <juandahurtadom@gmail.com>
2024-11-12 12:48:16 -0500

    Add renderer skeleton

--- Sources/Rea/Rea.swift
+++ Sources/Rea/Rea.swift
@@ -1,2 +1,26 @@
 // The Swift Programming Language
 // https://docs.swift.org/swift-book
+
+#if canImport(AppKit)
+import AppKit
+import Renderer
+import MetalKit
+
+public class ReaWindow: NSWindow {
+    var renderer = Renderer()
+    
+    override init(contentRect: NSRect, styleMask style: NSWindow.StyleMask, backing backingStoreType: NSWindow.BackingStoreType, defer flag: Bool) {
+        super.init(
+            contentRect: contentRect,
+            styleMask: style,
+            backing: backingStoreType,
+            defer: flag
+        )
+        
+        let metalView = MTKView()
+        contentView = metalView
+        metalView.delegate = renderer
+        metalView.colorPixelFormat = .rgba8Unorm
+    }
+}
+#endif
---
+++ Sources/Renderer/Renderer.swift
@@ -0,0 +1,17 @@
+//
+//  Renderer.swift
+//  Rea
+//
+//  Created by Juan Hurtado on 12/11/24.
+//
+
+import MetalKit
+
+public class Renderer: NSObject, MTKViewDelegate {
+    public func mtkView(_ view: MTKView, drawableSizeWillChange size: CGSize) {
+    }
+    
+    public func draw(in view: MTKView) {
+       // TODO: implement it lol
+    }
+}
