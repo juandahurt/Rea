82ce4a1c48764a7583c89488db2a172d9074adbe
Juan David Hurtado <juandahurtadom@gmail.com>
2024-11-15 15:42:15 -0500

    Demo fix (#4)
    * Add demo mac project
    
    * Remove old demo

---
+++ Demo/MacOS/MacOS/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  MacOS
+//
+//  Created by Juan David Hurtado on 15/11/24.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
---
+++ Demo/MacOS/MacOS/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  MacOS
+//
+//  Created by Juan David Hurtado on 15/11/24.
+//
+
+import Cocoa
+
+let appDelegate = AppDelegate()
+NSApplication.shared.delegate = appDelegate
+_ = NSApplicationMain(CommandLine.argc, CommandLine.unsafeArgv)
