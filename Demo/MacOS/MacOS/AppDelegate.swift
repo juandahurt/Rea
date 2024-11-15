f82080460410db840b375c64baad9888ae995985
Juan David Hurtado <juandahurtadom@gmail.com>
2024-11-15 15:37:12 -0500

    Add demo mac project

---
+++ Demo/MacOS/MacOS/AppDelegate.swift
@@ -0,0 +1,33 @@
+//
+//  AppDelegate.swift
+//  MacOS
+//
+//  Created by Juan David Hurtado on 15/11/24.
+//
+
+import Cocoa
+import Rea
+
+class AppDelegate: NSObject, NSApplicationDelegate {
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        let window = ReaWindow(
+            contentRect: .init(x: 0, y: 0, width: 800, height: 500),
+            styleMask: [.titled, .closable, .resizable],
+            backing: .buffered,
+            defer: false
+        )
+        
+        window.center()
+        window.title = "Rea Engine"
+        window.makeKeyAndOrderFront(nil)
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+    func applicationSupportsSecureRestorableState(_ app: NSApplication) -> Bool {
+        return true
+    }
+}
+
